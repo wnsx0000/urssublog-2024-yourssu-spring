6fe9122dd0d49c41efc209474170154e432e4939
wnsx0000 <wnsx0000@gmail.com>
2024-09-14 19:37:23 +0900

    240914,1937, first commit

---
+++ src/main/java/springproject/urssublog/UrssublogApplication.java
@@ -0,0 +1,12 @@
+package springproject.urssublog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrssublogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrssublogApplication.class, args);
+	}
+}
---
+++ src/test/java/springproject/urssublog/UrssublogApplicationTests.java
@@ -0,0 +1,13 @@
+package springproject.urssublog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrssublogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
