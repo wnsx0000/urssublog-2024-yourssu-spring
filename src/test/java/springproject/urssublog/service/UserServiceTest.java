e4467cc7316ffb1505f1b2ce639ba4f3eace0853
wnsx0000 <wnsx0000@gmail.com>
2024-09-16 19:35:55 +0900

    240916, 1935, User service and controller completed

--- src/test/java/springproject/urssublog/service/UserServiceTest.java
+++ src/test/java/springproject/urssublog/service/UserServiceTest.java
@@ -5,11 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 import springproject.urssublog.domain.User;
+import springproject.urssublog.exception.BlogResourceNotFoundException;
+import springproject.urssublog.exception.BlogUserNotFoundException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
 @SpringBootTest
@@ -49,6 +53,80 @@ public class UserServiceTest {
 
         //then
         assertThat(userService.getUsernameById(user.getId())).isEqualTo("username~");
+
+    }
+
+    /**
+     * User service 계층 회원 삭제 테스트 : 성공한 경우.
+     */
+    @Test
+    public void userDeleteSuccess() {
+        //given
+        User user = new User("email~", "password~", "username~");
+        userService.saveUser(user);
+        Long userId = user.getId();
+
+        //when
+        userService.deleteUser(userId);
+
+        //then
+        assertThat(userService.getUsernameById(userId)).isNull();
+    }
+
+    /**
+     * User service 계층 회원 삭제 테스트 : 회원 탈퇴(삭제) 시 해당 id의 회원이 존재하지 않는 경우. → BlogResourceNotFoundException
+     */
+    @Test
+    public void userDeleteNotFoundFailure() {
+        //given
+        Long wrongId = 10L;
+
+        //when, then
+        BlogResourceNotFoundException thrown = assertThrows(BlogResourceNotFoundException.class, () -> {
+            userService.deleteUser(wrongId);
+        });
+        log.debug("userDeleteNotFoundFailure(), no user to delete exception message={}", thrown.getMessage());
+    }
+
+    /**
+     * User service 계층 회원 로그인 테스트 : 성공한 경우.
+     */
+    @Test
+    public void userLoginSuccess() {
+        //given
+        User user = new User("email~", "password~", "username~");
+        userService.saveUser(user);
+
+        //when
+        User newUser = new User("email~", "password~");
+        User resultUser = userService.userLoginCheck(newUser);
+
+        //then
+        assertThat(resultUser).isEqualTo(user);
+    }
+
+    /**
+     * User service 계층 회원 로그인 테스트 : 로그인 시 회원 정보가 없는 경우. → BlogResourceNotFoundException
+     */
+    @Test
+    public void userLoginNotFoundFailure() {
+        //given
+        User user = new User("email~", "password~", "username~");
+        userService.saveUser(user);
+
+        //when
+        User wrongEmailUser = new User("!!!!!", "password~");
+        User wrongPasswordUser = new User("email~", "!!!!!");
+
+        //then
+        BlogUserNotFoundException  thrown1 = assertThrows(BlogUserNotFoundException .class, () -> {
+            userService.userLoginCheck(wrongEmailUser);
+        });
+        log.debug("userLoginNotFoundFailure(), wrong login email exception message={}", thrown1.getMessage());
+        BlogUserNotFoundException thrown2 = assertThrows(BlogUserNotFoundException .class, () -> {
+            userService.userLoginCheck(wrongEmailUser);
+        });
+        log.debug("userLoginNotFoundFailure(), wrong login password exception message={}", thrown2.getMessage());
 
     }
 }
